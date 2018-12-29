a7ea792dbbfab16ad3f52037529dcba846cf7702
jeffersonvital20 <jeffersonvital20@gmail.com>
2018-12-29 12:23:33 -0300

    fisrt commit

---
+++ cliente/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import persons_list, person_create, person_update, person_delete
+
+
+urlpatterns = [
+    path('list/', persons_list, name='person_list'),
+    path('new/', person_create, name='person_create'),
+    path('update/<int:id>/', person_update, name='person_update'),
+    path('delete/<int:id>/', person_delete, name='person_delete'),
+
+]
